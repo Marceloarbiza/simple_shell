eb12f66ef36daef466a8098d124bbeaac8e620bc
Marceloarbiza <3338@holbertonschool.com>
2021-08-25 15:04:37 -0700

    25-19:05

--- getEnvironCharN.c
+++ getEnvironCharN.c
@@ -39,9 +39,13 @@ char *compEnv()
 
 	len = _strlen(p[12] + 5);
 
-	pa = malloc(sizeof(char) * len);
+	pa = malloc(sizeof(char) * (len + 1));
+	if (!pa)
+		return (NULL);
 
 	_strcpy(pa, p[12] + 5);
+
+	pa[len] = '\0';
 
 	return (pa);
 }
